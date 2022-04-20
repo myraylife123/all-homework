4a500f21356ac7ac9fd2ed3dc63d0ea50fc3f510
myraylife123 <90734686+myraylife123@users.noreply.github.com>
2022-04-20 08:44:46 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -38,7 +38,7 @@ BOARD_MARGIN     = 2
 
 # Color declarations in the RGB notation
 WHITE    = (255,255,255)
-RED      = (255,0,0)
+RED      = (255,0,50)
 GREEN    = (0,255,0)
 BLUE     = (0,0,255)
 ORANGE   = (255,69,0)
